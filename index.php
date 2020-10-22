a076148f143e356edb7f0aeeaf0ec10f21036ca3
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-10-22 15:15:07 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -6,22 +6,24 @@
         <title>Web Animation Portfolio</title>
     </head>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <div class="w3-bar w3-blue w3-top">
-			<li class="w3-bar-item w3-button"><a href="index.php?page=home">Home</a></li>
-			<li class="w3-bar-item w3-button"><a href="index.php?page=module1">Module 1</a></li>
-			<li class="w3-bar-item w3-button"><a href="index.php?page=module2">Module 2</a></li>
-			<li class="w3-bar-item w3-button"><a href="index.php?page=module3">Module 3</a></li>
-			<li class="w3-bar-item w3-button"><a href="index.php?page=module4">Module 4</a></li>
-			<li class="w3-bar-item w3-button"><a href="index.php?page=module5">Module 5</a></li>
-			<li class="w3-bar-item w3-button"><a href="index.php?page=module6">Module 6</a></li>
+	<div class="w3-sidebar w3-bar-block w3-dark-blue w3-animate-left" style="display:none" id="mySidebar">
+		<button class="w3-bar-item w3-button w3-large"
+  			onclick="close()">Close &times;</button>
+			<a class="w3-bar-item w3-button" href="index.php?page=home">Home</a></li>
+			<a class="w3-bar-item w3-button" href="index.php?page=module2">Module 2</a></li>
+			<a class="w3-bar-item w3-button" href="index.php?page=module3">Module 3</a></li>
+			<a class="w3-bar-item w3-button" href="index.php?page=module4">Module 4</a></li>
+			<a class="w3-bar-item w3-button" href="index.php?page=module5">Module 5</a></li>
+			<a class="w3-bar-item w3-button" href="index.php?page=module6">Module 6</a></li>
 	</div>
+	<div>
+		<button class="w3-button w3-white w3-xxlarge" onclick="open()">&#9776;</button>
+	<div class="w3-container">
+	<h1>Hans Toquero Portfolio</h1>
     <?php
 	switch($load){
 		case 'home':
 			require_once('home.php');
-		break;
-		case 'module1':
-			require_once('module_1.php');
 		break;
 		case 'module2':
 			require_once('module_2.php');
@@ -42,4 +44,14 @@
 			require_once('home.php');
 	}
 ?>
+	</div>
+	</div>
+<script>
+function open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+function close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
+</script>
 </html>
