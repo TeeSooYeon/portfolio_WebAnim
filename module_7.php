bd466a3a644a48927a21ae34e538db9258f2fda2
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-11-25 14:51:04 +0800

    Create module_7.php

---
+++ module_7.php
@@ -0,0 +1,32 @@
+<html>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <div class="w3-container">
+        <div class="w3-container w3-grey">
+            <h2>Learning Task 7.1</h2>
+	    </div>
+       <div style="position: absolute; top: 50px; left: 50px; background-color: rgb(193, 241, 225);"><canvas id="hat" width="200" height="200"></canvas>
+        </div>
+        <div style="position: absolute; top: 50px; left: 300px; background-color: rgb(193, 241, 225);"><canvas id="wine" width="200" height="200"></canvas>
+        </div>
+        <div style="position: absolute; top: 50px; left: 550px; background-color: rgb(193, 241, 225);"><canvas id="radio" width="200" height="200"></canvas>
+        </div>
+        <div style="position: absolute; top: 50px; left: 800px; background-color: rgb(193, 241, 225);"><canvas id="corndog" width="200" height="200"></canvas>
+        </div>
+        <div style="position: absolute; top: 300px; left: 300px; background-color: rgb(193, 241, 225);"><canvas id="lemonade" width="200" height="200"></canvas>
+        </div>
+        <div style="position: absolute; top: 300px; left: 550px; background-color: rgb(193, 241, 225);"><canvas id="heart" width="200" height="200"></canvas>
+        </div>
+        <div style="position: absolute; top: 300px; left: 800px; background-color: rgb(193, 241, 225);"><canvas id="card" width="200" height="200"></canvas>
+        </div>
+        <div style="position: absolute; top: 300px; left: 50px; background-color: rgb(193, 241, 225);"><canvas id="dedhead" width="200" height="200"></canvas>
+        </div>
+    </div>
+</html>
+<script type="text/javascript" src="jsC/hat.js"></script>
+<script type="text/javascript" src="jsC/radio.js"></script>
+<script type="text/javascript" src="jsC/wine.js"></script>
+<script type="text/javascript" src="jsC/corndog.js"></script>
+<script type="text/javascript" src="jsC/lemonade.js"></script>
+<script type="text/javascript" src="jsC/heart.js"></script>
+<script type="text/javascript" src="jsC/card.js"></script>
+<script type="text/javascript" src="jsC/dedhead.js"></script>
